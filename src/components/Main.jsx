b06876cd052dbfef49f14e980753bba8c6a21f20
Simone <info@simoneicardi.com>
2025-02-12 12:32:03 +0100

    dividi in macro componenti

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+// Main.jsx
+export default function Main() {
+    return (
+        <main>
+            <section id="products">
+                <h2>Lista prodotti</h2>
+            </section>
+        </main>
+    );
+}
