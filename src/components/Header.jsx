2c8fde8368beca16e0845b728f93204214754e76
Simone <info@simoneicardi.com>
2025-02-13 12:56:46 +0100

    passaggio in props da Header a Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,19 @@ import Navbar from "./Navbar.jsx";
 
 
 export default function Header(props) {
+
+    // dati ricavati per la gestione dei link della Navbar
+    const links = [
+        { id: 1, text: 'Home', url: '#', current: false },
+        { id: 2, text: 'Prodotti', url: '#', current: true },
+        { id: 3, text: 'Chi Siamo', url: '#', current: false },
+        { id: 4, text: 'Contatti', url: '#', current: false },
+    ];
+
     return (
         <header>
             <Logo slogan={props.slogan} />
-            <Navbar />
+            <Navbar linksProp={links} />
         </header>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,13 @@
 export default function Navbar(props) {
 
-    // dati ricavati per la gestione dei link della Navbar
-    const links = [
-        { id: 1, text: 'Home', url: '#', current: false },
-        { id: 2, text: 'Prodotti', url: '#', current: true },
-        { id: 3, text: 'Chi Siamo', url: '#', current: false },
-        { id: 4, text: 'Contatti', url: '#', current: false },
-    ];
+    // const links = props.linksProp;
+    // versione desstructuring
+    // const {linksProp} = props;
 
     return (
         <nav>
             <ul>
-                {links.map((link) => (
+                {props.linksProp.map((link) => (
                     <li key={link.id}>
                         <a href={link.url} className={link.current ? 'active' : ''}>
                             {link.text}
