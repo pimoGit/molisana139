f03c7a1ae78650d3c54b5a152c6e97907be64ae0
Simone <info@simoneicardi.com>
2025-02-17 10:49:58 +0100

    uso avanzato counter usestate con callback

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
+
+
 const Counter = () => {
 
-    function saluta() {
-        alert("ciao");
+    // utilizzo dello useState per la variabile di conteggio
+    const [count, setCount] = useState(0);
+
+    function incrementa() {
+        setCount(count + 1);
+        setCount(count + 1);
+        // setCount((current) => current + 1);
+        // setCount((current) => current + 1);
     }
 
     return (
-        <div>
-            <button onClick={saluta}>Saluta</button>
+        <div className="container">
+            <h1>Contatore</h1>
+            <p className="count">{count}</p>
+            <div className="count__buttons">
+                <button className="btn" onClick={incrementa}>
+                    +
+                </button>
+                <button className="btn" onClick={() => setCount(count - 1)}>
+                    -
+                </button>
+            </div>
         </div>
     )
 }
