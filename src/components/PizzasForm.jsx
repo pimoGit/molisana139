8b2eec7738fdcd4a162d2bd0060309ddf81c1639
Simone <info@simoneicardi.com>
2025-02-19 11:51:08 +0100

    gestione completa inserimento nuova pizza

--- src/components/PizzasForm.jsx
+++ src/components/PizzasForm.jsx
@@ -41,18 +41,91 @@ const initialMenuData = [
     }
 ];
 
+
+const initialFormData = {
+    name: "",
+    image: "",
+    description: "",
+    price: 0,
+    available: false,
+};
+
 const PizzasForm = () => {
 
     // utilizzo dello useState per la gestione dei data (array degli oggetti pizza)
     const [menu, setMenu] = useState(initialMenuData);
     // state per la gestione delle informazioni raccolte dai campi del form
-    // const [formData, setFormData] = useState(initialFormData);
+    const [formData, setFormData] = useState(initialFormData);
 
 
+    // funzione di gestione delle info dei campi
+    function handleFormData(e) {
+        // gestione del value a seconda del tipo di input
+        const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+        // setta tramite setState l'oggetto con le info prese dai campi del form
+        setFormData((currentFormData) => ({
+            ...currentFormData,
+            [e.target.name]: value,
+        }));
+    }
+
+    // funzione di gestione dell'invio dell'intero form (tuue le info dei vari campi)
+    function handleSubmit(e) {
+        e.preventDefault();
+        setMenu((currentMenu) => [...currentMenu, { id: currentMenu[currentMenu.length - 1].id + 1, ...formData }]);
+        // resetto il form
+        setFormData(initialFormData);
+    }
 
     return (
         <>
             <h1>questo e il form delle pizze</h1>
+
+            <form id='formpizza' action="#" onSubmit={handleSubmit}>
+                {/* valore nome pizza */}
+                <input
+                    type="text"
+                    name="name"
+                    onChange={handleFormData}
+                    value={formData.name}
+                    placeholder='Nome pizza'
+                />
+                {/* valore immagine */}
+                <input
+                    type="text"
+                    name="image"
+                    onChange={handleFormData}
+                    value={formData.image}
+                    placeholder='Imagine pizza'
+                />
+                {/* valore descrizione */}
+                <textarea
+                    name="description"
+                    onChange={handleFormData}
+                    value={formData.description}
+                    placeholder='Descrizione pizza'
+                ></textarea>
+                {/* valore prezzo */}
+                <input
+                    type="number"
+                    name="price"
+                    onChange={handleFormData}
+                    value={formData.price}
+                    placeholder='Prezzo pizza'
+                />
+                {/* valore disponibilità */}
+                <label htmlFor="available">Disponibile</label>
+                <input
+                    type="checkbox"
+                    name="available"
+                    checked={formData.available}
+                    onChange={handleFormData}
+                    id="available"
+                />
+                {/* bottone di invio info */}
+                <button>Aggiungi</button>
+            </form>
 
             {
                 menu.map((pizza) => (
